afd500384ddd39e59f30564cf41c0d72ac45261e
Elfing Elf <74399251+JulijaGC@users.noreply.github.com>
2021-03-30 15:17:59 +0100

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Viking Knit Bracelet',
+            category:'Bracelets',
+            image:'/images/p1.jpg',
+            price:56,
+            brand:'ElfingElf',
+            rating:5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Viking Knit Bracelet',
+            category:'Bracelets',
+            image:'/images/p1.jpg',
+            price:46,
+            brand:'ElfingElf',
+            rating:5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'3',
+            name:'Viking Knit Bracelet',
+            category:'Bracelets',
+            image:'/images/p1.jpg',
+            price:36,
+            brand:'ElfingElf',
+            rating:5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'4',
+            name:'Viking Knit Bracelet',
+            category:'Bracelets',
+            image:'/images/p1.jpg',
+            price:16,
+            brand:'ElfingElf',
+            rating:5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'5',
+            name:'Viking Knit Bracelet',
+            category:'Bracelets',
+            image:'/images/p1.jpg',
+            price:26,
+            brand:'ElfingElf',
+            rating:5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'6',
+            name:'Viking Knit Bracelet',
+            category:'Bracelets',
+            image:'/images/p1.jpg',
+            price:36,
+            brand:'ElfingElf',
+            rating:5,
+            numReviews:10,
+            description:'high quality product'
+        },
+    ],
+};
+export default data;
